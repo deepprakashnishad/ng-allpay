a693b7d2ec067754eff99a885b93211a7ba529c0
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-08-04 22:53:50 +0530

    Minor fixef

--- src/app/reports/report.component.ts
+++ src/app/reports/report.component.ts
@@ -10,6 +10,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import {FormControl} from '@angular/forms';
 import { NotifierService } from 'angular-notifier';
 import { ReportService } from './report.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { environment } from './../../environments/environment';
 import {
   MatBottomSheet,
@@ -20,15 +21,37 @@ import {
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class ReportComponent implements OnInit, AfterViewInit {
 
   startDate: string;
   endDate: string;
   transactions: Array<any> = [];
+  totalAmt: number = 0;
 
-  displayedColumns: string[] = ['bp', 'm', 'pg','a','poid', 'puid', 'pum', 'pun', 'pm', 'pd', 'actions', 'status'];
+  availableColumns: any[] = [
+    {"value": "bp", displayName: "Betting Partner"},
+    {"value": "m", displayName: "Merchant"},
+    {"value": "pg", displayName: "Payment Gateway"},
+    {"value": "a", displayName: "Amount"},
+    {"value": "poid", displayName: "Order Id"},
+    {"value": "puid", displayName: "User Id"},
+    {"value": "pum", displayName: "User Mobile"},
+    {"value": "pun", displayName: "Username"},
+    {"value": "pm", displayName: "Payment Mode"},
+    {"value": "pd", displayName: "Payment Detail"},
+    {"value": "actions", displayName: "Actions"},
+    {"value": "s", displayName: "Status"}
+  ];
+  displayedColumns: string[] = ['bp', 'm', 'pg','a','poid', 'pun', 'pm', 's'];
   dataSource: MatTableDataSource<any>;
 
   @ViewChild('paginator') paginator: MatPaginator;
@@ -73,6 +96,10 @@ export class ReportComponent implements OnInit, AfterViewInit {
           this.transactions.push(...result.txns);
         }
 
+        this.totalAmt = this.transactions.reduce((accumulator, object) => {
+          return accumulator + object.a;
+        }, 0);
+
         this.dataSource = new MatTableDataSource<any>(this.transactions)
         this.dataSource.sort = this.sort;
       }else{
@@ -81,5 +108,32 @@ export class ReportComponent implements OnInit, AfterViewInit {
     })
   }
 
-  sortData(sort: MatSort) { this.dataSource.sort = this.sort; }
+  sortData(sort: MatSort) { 
+    console.log(sort);
+    var transactions = this.transactions.sort(function(a, b) {
+      var keyA = a[sort['active']],
+      keyB = b[sort['active']];
+      if(sort['active']==="bp" || sort['active']==="m"){
+        keyA = keyA['name'];
+        keyB = keyB['name'];
+      }
+      if(sort['direction']==="asc"){
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+      }else if(sort['direction']==="desc"){
+        if (keyA < keyB) return 1;
+        if (keyA > keyB) return -1;
+      }
+      return 0;
+    });
+    this.dataSource.data = transactions; 
+  }
+
+  toggleRow(element: { expanded: boolean; }) {
+    // Uncommnet to open only single row at once
+    // ELEMENT_DATA.forEach(row => {
+    //   row.expanded = false;
+    // })
+    element.expanded = !element.expanded
+  }
 }
